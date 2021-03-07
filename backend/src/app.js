adc0eb8f2943f2b2624a29f81fbf4e585afec342
Edwin Sanchez <edwinsanchez750@gmail.com>
2021-03-06 22:32:16 -0500

    Added the possibility of decrease the deprecated level of a point given its latitude and longitude

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,6 +10,6 @@ app.use(cors())
 app.use(express.json())
 
 //routes
-app.use("/api/points", require('./routes/pointRoutes'))
+app.use("/", require('./routes/pointRoutes'))
 
 module.exports = app;
