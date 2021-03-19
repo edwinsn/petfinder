96c1077fd6e0226bdcb574eec8e01b139f45bb41
Edwin Sanchez <edwinsanchez750@gmail.com>
2021-03-18 22:20:50 -0400

    GetMarkers component saves the downloaded markers so the point controller does not send them again, besides only the markers that can be seen by the user are requested. Minor changes in the css.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React, {Component} from 'react';
-import { MapContainer, TileLayer, useMap, useMapEvents} from "react-leaflet";
-import L from 'leaflet'
+import { MapContainer, TileLayer, ZoomControl} from "react-leaflet";
 import Search from "./Search";
 import './assets/App.css';
 import {Animal} from './Animal'
 import {Panel} from './Panel'
 import {About} from './About'
-import axios from 'axios'
-import dogIcon from './assets/images/dogIcon.png'
-import catIcon from './assets/images/catIcon.png'
+import {GetMarkers} from './GetMarkers'
+
 
 
 export class App extends Component{
@@ -29,14 +27,15 @@ export class App extends Component{
     return (
       <div >
         <div>
-          <MapContainer id="map" style={{"zIndex":0}} center={{ lat: 5.533, lng: -73.367}} zoom={16} zoomControl={false}>
+          <MapContainer className="mapp" id="map" style={{"zIndex":0}} center={{ lat: 5.533, lng: -73.367}} zoom={16} zoomControl={false}>
             <TileLayer
               attribution="&copy; <a href='https://osm.org/copyright'>OpenStreetMap</a> contributors"
               url={"https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"}>
             </TileLayer>
+            <ZoomControl position="bottomleft" />
             <Search className="search"/>
-            <Animal panelDisplay={this.openPanel}/>
-            <GetMarkers  open={this.openPanel} />     
+            <Animal panelDisplay={this.openPanel}/>   
+            <GetMarkers open={this.openPanel}/>
             <About className="about"></About> 
           </MapContainer>
         </div>
@@ -44,59 +43,4 @@ export class App extends Component{
       </div>
     );
   }
-}
-
-
-let GetMarkers = React.memo((props)=>{
-
- console.log("Get markers rendered")
-
-  let map = useMap ();
-  map.locate()
-
-  useMapEvents({
-    locationfound(e) {
-      map.panTo(e.latlng, map.getZoom())
-    }
-  })
- 
-  axios.get(process.env.REACT_APP_POINTS_URI+"/database").then((res)=>{
-      if(res.status===200){
-
-      res.data.forEach(marker => {
-
-        var icon = new L.Icon({
-        iconUrl: marker.type==="dog"?dogIcon:catIcon,
-
-        iconSize: [35, 35]
-        })
-
-        let newmarker = L.marker({lat:marker.lat,lng:marker.lng},{icon});
-        newmarker.addTo(map)
-        newmarker.on("click",()=>{
-          props.open(marker.lat, marker.lng, marker.deprecated_level)
-        })
-      });
-      }else{
-        console.log("Db error")
-      } 
-    }
-  )
-   return null
-}
-)
-
-let GetMarkersbyLocation = ()=>{
-
-  let lat,lng = [0,0]
-  let earthRadious = 6371
-  let relativeRadious = earthRadious*Math.cos(lat)
-  let markslng = 10/relativeRadious
-  let markslat = 10/earthRadious
-  /*axios.get(process.env.REACT_APP_POINTS_URI,*/
-    console.log({minlat:lat-markslat,
-     maxlat:lat+markslat,
-     minlng:lng-markslng,
-     maxlng:lng+markslng
-    })
 }
