690da0f5ef9220e9f3ed1fbbb283a3be54db656e
Edwin Sanchez <edwinsanchez750@gmail.com>
2021-10-28 02:15:00 -0500

    Added styled-components to AboutRec.js

--- frontend/src/AboutRec.js
+++ frontend/src/AboutRec.js
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react'
 import './assets/AboutRec.css'
 import animalsIcon from './assets/images/animalsIcon.png'
+import styled from 'styled-components'
+
 
 export function About() {
 
@@ -13,18 +15,60 @@ export function About() {
     }, [])
 
     return (
-        <div id={"about"} className="rounded centered" style={{ backgroundColor: visible ? "white" : undefined }}>
-
-            {!visible && <div id="infoIcon" className="bolder white">i</div>}
-            <div className={visible ? "" : "hide"} id="info">
-                <p>Encuentra y reporta y atuda animales <span><img src={animalsIcon}></img></span> en la calle</p>
-                <ul >
+        <StyledAbout
+            className="rounded centered pointer"
+            deployed={visible}
+        >
+            <InfoIcon id="infoIcon" className="bolder white">i</InfoIcon>
+            <Info id="info">
+                <p>Encuentra y reporta y atuda animales <span><Img src={animalsIcon}></Img></span> en la calle</p>
+                <Ul>
                     <li>Agrega un registro dando click en <span className="blue bolder">+</span></li>
                     <li>Arrastra la marca que agregaste </li>
                     <li>Ajusta el <span className="green bolder">área</span> de residencia</li>
                     <li>Puedes incluir una <span className="blue">foto </span>y otros datos </li>
-                </ul>
-            </div>
+                </Ul>
+            </Info>
 
-        </div>)
+        </StyledAbout>)
 }
+
+const StyledAbout = styled.div`
+    font-style: italic;
+    padding: 0 12px;
+    font-family: 'Roboto';
+    min-width: 0.25cm;
+    z-index: 408;
+    position: absolute;
+    left: 0;
+    min-height: 34px;
+    margin-top: 45px;
+    margin-left: 10px;
+    background-color:${props => props.deployed ? "white" : "#3388FF"};
+    &:hover {
+        background-color: white;
+    }
+    &:hover #infoIcon {
+    display: none;
+    }
+    &:hover #info{
+        display: block;
+    }
+`
+
+const InfoIcon = styled.div`
+    font-size: 19px;
+`
+
+const Info = styled.div`
+    display: none;
+`
+
+const Ul = styled.ul`
+    padding-left: 20px;
+`
+
+const Img = styled.img`
+    height: 2em;
+    position: relative;
+    top: 0.8em;`
