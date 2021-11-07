21170e209877a92b25b0e5f6f549015fbe94d14a
Edwin Sanchez <edwinsanchez750@gmail.com>
2021-11-06 21:41:17 -0500

    Updated pointBU to use Styled components

--- frontend/src/pointBU.js
+++ frontend/src/pointBU.js
@@ -1,10 +1,12 @@
-import './assets/pointBU.css'
 import dogIcon from './assets/images/dogIcon.svg'
 import catIcon from './assets/images/catIcon.svg'
-import deployIcon from './assets/images/deployIcon.svg'
-import { useState } from 'react'
+import deployIconImg from './assets/images/deployIcon.svg'
+import { useState, useMemo, useCallback } from 'react'
 import { useDispatch } from 'react-redux'
 import { triggerBackup } from './features/recoverSlice'
+import styled, { css } from 'styled-components'
+
+//optimizar para cuando no hay imagen
 
 export let PointBu = (props) => {
 
@@ -15,18 +17,27 @@ export let PointBu = (props) => {
 
     const dispatch = useDispatch()
 
-    let probabilityBars = []
-    if (true) {
+    let probabilityBars = useMemo(() => {
+        let bars = []
         for (let i = 0; i < 10; i++) {
-            probabilityBars.push(
+            bars.push(
                 <div key={Math.random()} style={{ background: i < frecuence ? "#3B72E1" : "#E3E3E3" }}></div>
             )
         }
+        return bars
+    }, [frecuence])
+
+    let toggleDeploy = useCallback(() => {
+        setDeployed(prev => !prev)
+    }, [])
+
+    let restoreMark = () => {
+        dispatch(triggerBackup({ ...props.mark, coords: { lat: props.mark.lat, lng: props.mark.lng } }))
+        setRecovered(true)
     }
 
-
     return recovered ? null : (
-        <div className={deployed ? "point-bu pointDeployed " : "point-bu nodeployed"}>
+        <DropdownMenu deployed={deployed}>
             <div
                 onClick={() => { setDeployed(true) }}
             >
@@ -34,22 +45,92 @@ export let PointBu = (props) => {
                 <div >{description || "Sin descripción"}</div>
             </div>
             {deployed &&
-                <div className="aditionalInfo">
+                <AditionalInfo>
                     <div className="probabilitybars">
                         {probabilityBars}
                     </div>
-                    {imgurl && <img src={imgurl} />}
-                </div>}
-            <button
-                onClick={() => {
-                    dispatch(triggerBackup({ ...props.mark, coords: { lat: props.mark.lat, lng: props.mark.lng } }))
-                    setRecovered(true)
-                }}
-                className={deployed ? "resbtn" : "resbtn"}>Restaurar</button>
-            <img className={deployed ? "deployIcon turn" : "deployIcon return"}
-                src={deployIcon}
-                onClick={() => { setDeployed(!deployed) }}
+                    {imgurl && <AnimalPhoto src={imgurl} />}
+                </AditionalInfo>}
+            <RestoreBtn
+                onClick={restoreMark}
+            >Restaurar</RestoreBtn>
+            <DeployIcon
+                deployed={deployed}
+                src={deployIconImg}
+                onClick={toggleDeploy}
             />
-        </div >
+        </DropdownMenu >
     )
 }
+
+let DropdownMenu = styled.div`
+    align-items: center;
+    display: flex;
+    background-color: hsl(0, 0%, 97%);
+    transition: height var(--transition-time);
+    max-height: 10cm;
+    padding: 7px;
+    border-radius: 5px;
+    margin-bottom: 3px;
+    ${props => {
+        return props.deployed ?
+            css`
+            height: 40vh;
+            flex-wrap: wrap;
+            justify-content: space-evenly;
+            &>div:nth-child(1){width:100%}
+            `:
+            css`
+                justify-content: space-evenly;
+                height: 7vh;
+                white-space: nowrap;`}
+    }
+}
+    &>div {
+        display: flex;
+        overflow: hidden;
+        max-height: 80%;
+    &>div>img {
+        align-self: center;
+    }
+    &>div:nth-child(1) {
+    flex-grow: 1;
+    }
+    `
+
+let AditionalInfo = styled.div`
+   align-items: center;
+    width: 95%;
+    display: flex;
+    flex-direction: column;
+`
+
+let DeployIcon = styled.img`
+    animation-fill-mode: forwards;
+    animation-duration: 400ms;
+    width: 2.8vh;
+    margin-left: 5px;
+    ${props => {
+        return props.deployed ?
+            css`
+    animation-name: turn;
+    `:
+            css`   
+    animation-name: return;
+        `
+    }}
+`
+
+let AnimalPhoto = styled.img`
+    min-height: 20vh ;
+    background-color: rgb(219, 219, 219);
+    width: 100%;
+`
+
+let RestoreBtn = styled.button`
+    background-color: transparent;
+    border: 3px solid green;
+    color: green;
+    cursor: pointer;
+    border-radius: var(--border-radius);
+    `
